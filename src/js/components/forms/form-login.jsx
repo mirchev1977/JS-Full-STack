3ccc81d69de18e4da4fc1092ad9ad90412e47e45
Miroslav <mirchev1977@novihorizonti.eu>
2016-07-30 11:55:23 +0300

    users can now login

--- src/js/components/forms/form-login.jsx
+++ src/js/components/forms/form-login.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import jQuery from 'jquery';
 
 
 export default class FormLogin extends React.Component{
 
 
 	render(){
-		
 		return (<div className="form-login">
 			<h1>Hello World From The Login Form.</h1>
-			<form>I am the form element</form>
+			<form onSubmit={this._handleSubmit.bind(this)} >
+				<div className="field-input">
+				<label>Email:
+				<input type="text" ref={(input) => this._email = input} /></label></div>
+				<div className="field-input">
+				<label>Password:
+				<input type="password" ref={(input) => this._password = input} /></label></div>
+				<input type="submit" value="Submit" />
+			</form>
 		</div>);
+	}
+
+
+	_handleSubmit(event){
+		event.preventDefault();
+		let email = this._email.value;
+		let password = this._password.value;
+		this._login(email, password);
+	}
+
+	_login(email, password){
+		jQuery.ajax({
+		  method: 'POST',
+		  url: '/api/users/login',
+		  headers: {
+                'Authorization': 'bearerU2FsdGVkX185JuJ70Oq38E0Y4Ip96ozN2/kHBGkL2lwUQtbSfdOoVA7'+
+                '2oFCvAQL9lYcIJdrNvOuOHt37UtQRUaqEuHKCuyeVS9o35/j4EMo6vhA2sx13yIQDg9ZSZsVc'
+            },
+            data: "email=" + email + "&password=" + password,
+		  success: () => {
+		  	window.location.replace('/courses');
+		  }
+		});
+	}
+
+	componentWillMount(){
+		this._login();
 	}
 }
